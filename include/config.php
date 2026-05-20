9a4fb3423288717b4f2a50a87f4ddab3baf5280c
Vovashh <shmug.vova@gmail.com>
2026-05-20 12:33:43 +0300

    1

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "game_db"; // Назва твоєї бази
+$servername = "sql101.infinityfree.com";
+$username = "if0_41972489";
+$password = "rJ2BRmNfBv";
+$dbname = "if0_41972489_game_db";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
